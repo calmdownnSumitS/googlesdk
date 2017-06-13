227b55e7d68fd4c196f4a44a5f0bed88d6b3bc7b
macserver <macserver@macservers-mac-pro.local>
2017-06-13 15:12:57 +0530

    Initial Commit

---
+++ googleintegrate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  googleintegrate
+//
+//  Created by macserver on 6/13/17.
+//  Copyright © 2017 macserver. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
